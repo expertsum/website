5111fd37d4331c7fe827344d339360e61fb3532c
Enrique Lopez de Lara <elopezdelara@expertsum.com>
2017-04-22 12:59:45 -0400

    Changed python runtime + changes to the structure and content of the website

---
+++ www/js/script.js
@@ -0,0 +1,35 @@
+/* 
+ * Enrique Lopez de Lara
+ * Expertsum Solutions
+ * 
+*/
+
+$(document).ready(function() {
+  $(_.first(_.shuffle($('.recommendation')))).show();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
